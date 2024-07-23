410bec4988bc92ebf7d28c71fafdb1c8543f70b1
ilaumjd <ilaumjd@gmail.com>
2024-07-23 10:51:07 +0700

    add rust

---
+++ rust/shell.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/f7207adcc68d9cafa29e3cd252a18743ae512c6a.tar.gz";
+}) {} }:
+
+pkgs.mkShell {
+  buildInputs = [ pkgs.cargo pkgs.rustc ];
+}
